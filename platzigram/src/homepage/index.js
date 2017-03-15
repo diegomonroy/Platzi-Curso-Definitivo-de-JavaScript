65737096d7b81dbaa4aa5218c817d674204b4f0d
Diego Fernando Monroy Muñoz <dmsoftwaresas@gmail.com>
2017-03-14 20:37:35 -0400

    Checkpoint in Platzigram.

--- platzigram/src/homepage/index.js
+++ platzigram/src/homepage/index.js
@@ -6,35 +6,35 @@ var request = require('superagent');
 var header = require('../header');
 var axios = require('axios');
 
-page('/', header, loadPicturesFetch, function (ctx, next) {
+page('/', header, asyncLoad, function (ctx, next) {
 	title('Platzigram');
 	var main = document.getElementById('main-container');
 	
 	empty(main).appendChild(template(ctx.pictures));
 })
 
-// function loadPictures(ctx, next) {
-// 	request
-// 		.get('/api/pictures')
-// 		.end(function (err, res) {
-// 			if (err) return console.log(err);
+function loadPictures(ctx, next) {
+	request
+		.get('/api/pictures')
+		.end(function (err, res) {
+			if (err) return console.log(err);
 
-// 			ctx.pictures = res.body;
-// 			next();
-// 		});
-// }
+			ctx.pictures = res.body;
+			next();
+		});
+}
 
-// function loadPicturesAxios(ctx, next) {
-// 	axios
-// 		.get('/api/pictures')
-// 		.then(function (res) {
-// 			ctx.pictures = res.data;
-// 			next();
-// 		})
-// 		.catch(function (err) {
-// 			console.log(err);
-// 		});
-// }
+function loadPicturesAxios(ctx, next) {
+	axios
+		.get('/api/pictures')
+		.then(function (res) {
+			ctx.pictures = res.data;
+			next();
+		})
+		.catch(function (err) {
+			console.log(err);
+		});
+}
 
 function loadPicturesFetch(ctx, next) {
 	fetch('/api/pictures')
@@ -48,4 +48,13 @@ function loadPicturesFetch(ctx, next) {
 		.catch(function (err) {
 			console.log(err);
 		})
+}
+
+async function asyncLoad(ctx, next) {
+	try {
+		ctx.pictures = await fetch('/api/pictures').then(res => res.json());
+		next();
+	} catch (err) {
+		return console.log(err);
+	}
 }
